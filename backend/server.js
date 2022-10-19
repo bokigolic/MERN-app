ac6650da665dd7159918558e83629b5aaa0bf629
bokigolic <bokigolic32@gmail.com>
2022-10-19 10:18:45 -0400

    routes setup, postman works

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const dotenv = require ('dotenv').config()
+const dotenv = require('dotenv').config()
 const port = process.env.PORT || 500
 
 const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
