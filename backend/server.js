b3c489c58919304874d2eae4981e5f5335ff0e38
bokigolic <bokigolic32@gmail.com>
2022-10-19 01:20:24 -0400

    setup port, server running

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-console.log("This is run server test")
+const express = require('express')
+const dotenv = require ('dotenv').config()
+const port = process.env.PORT || 500
+
+const app = express()
+app.listen(port, () => console.log(`Server started on port ${port}`))
